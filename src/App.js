104a2e75f4fb04eef4d7394581aaf8850d9fd006
GopiUppu <gopiv3771@gmail.com>
2025-02-12 14:22:55 +0530

    added a image

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <h1>OnePlus 13 is available at Rs 5,000 discount but this offer is only available for a limited time</h1>
+      <img src="https://img-s-msn-com.akamaized.net/tenant/amp/entityid/AA1yNIZ3.img?w=768&h=432&m=6"></img>
     </div>
   );
 }
