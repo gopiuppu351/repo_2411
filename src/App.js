74f79727c31513c8beb5aa69d25a14dbbe56ff5f
gopiuppu351 <gopiv3771@gmail.com>
2025-02-12 14:30:40 +0530

    Update App.js
    added paragraph

--- src/App.js
+++ src/App.js
@@ -6,6 +6,28 @@ function App() {
     <div className="App">
       <h1>OnePlus 13 is available at Rs 5,000 discount but this offer is only available for a limited time</h1>
       <img src="https://img-s-msn-com.akamaized.net/tenant/amp/entityid/AA1yNIZ3.img?w=768&h=432&m=6"></img>
+      <p>OnePlus is hosting a Red Rush sale, which the company says is in "initiative for the Indian community". 
+         The sales began on February 11, and will go on till February 16. The sale offers are available both online and 
+         offline, including on the OnePlus India website, OnePlus Store App, OnePlus experience stores, Amazon India and 
+         the company's mainline partners such as Reliance Digital, Croma, Vijay Sales, Bajaj Electronics, and others. 
+         The sale is interesting because the company is offering discounts, deals and bank offers on fairly new devices, 
+         including the OnePlus 13, OnePlus 13R, OnePlus Pad 2, and the OnePlus Watch 2R, among others. Of these, we find the 
+         deal on the OnePlus 13 particularly good.OnePlus says that during the Red Rush sale, customers will be able to get an 
+         instant bank discount of Rs 5,000 on the purchase of the OnePlus 13. This is valid only on select bank cards 
+         including ICICI Bank, OneCard, RBL Bank, and HDFC Bank. With the bank discount, the price of the OnePlus 13 comes 
+         down to Rs 64,999 from 69,999 for the base 12GB RAM and 256GB storage variant. The same discount also applied to 
+         the 16GB RAM and 512GB storage model, bringing its price down from Rs 76,999 to Rs 71,999.In addition to that, 
+         if you have an old smartphone that you are willing to exchange, you will be eligible for up to Rs 7,000 discount,
+         on top of the bank offer. For instance, if you have a OnePlus 10 Pro, you can get up to Rs 20,810 at the time of 
+         exchange. Which means, the price of the OnePlus 13 after exchange would come down to Rs 44,189. You can exchange 
+         smartphones of any brand, it does not necessarily have to be a OnePlus phone.The OnePlus 13 features a 6.82-inch 
+         120Hz QHD+ display with Dolby Vision support, 1,600nits local brightness, and 4,500nits of peak brightness. 
+         It is powered by the Qualcomm Snapdragon 8 Elite chipset, built on a 3nm process, which is paired with 
+         LPDDR5X RAM, and UFS 4.0 storage. Running the phone is OxygenOS 15 based on Android 15. The OnePlus 13 comes with a 
+         promise of four years of major Android updates and five years of security patches.Fuelling the OnePlus 13 is a 
+         6,000mAh battery with support for 100W wired charging and 50W wireless charging. For photography, the OnePlus 13 
+         uses a 50-megapixel LYT-808 main camera, along with a 50-megapixel telephoto and ultrawide sensors. 
+         It features a 32-megapixel front camera. </p>
     </div>
   );
 }
