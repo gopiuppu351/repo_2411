06a4f01cb1e771090c6b2d7272a4c8e42315828d
GopiUppu <gopiv3771@gmail.com>
2025-02-12 14:19:18 +0530

    initial files are added

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>This line is added by some student</h1>
+      <h1>This line is added by Manjunadh</h1>
+    </div>
+  );
+}
+
+export default App;
